32746f01aa65291ca67873d63027604d86f2fe95
Stefano.In91 <ste.ince91@gmail.com>
2022-12-14 17:00:24 +0100

    adds casting to parking info

--- index.php
+++ index.php
@@ -38,6 +38,8 @@
     ],
   ];
 
+  $parking = $_GET["parking"];
+
 ?>
 
 <!DOCTYPE html>
@@ -46,12 +48,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hotels</title>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   </head>
   <body>
-    <main class="d-flex justify-content-center align-items-center mt-5">
+    <header class="m-5">
+      <form action="index.php">
+        <div class="form-group">
+          <label for="parking">Parcheggio Integrato?</label>
+          <select name="parking" id="parking" class="form-control mt-2 mb-2">
+            <option value="both">Entrambi</option>
+            <option value="true">Si</option>
+            <option value="false">No</option>
+          </select>
+          <button class="btn btn-dark">Invia</button>
+        </div>
+      </form>
+    </header>
+    <main class="m-5">
       <table class="table table-dark">
         <thead>
           <tr>
@@ -63,15 +78,26 @@
           </tr>
         </thead>
         <tbody>
-          <?php for($i=0; $i < count($hotels); $i++) { ?>
-            <tr>
-              <th> <?php echo $hotels[$i]["name"]; ?></td>
-              <td> <?php echo $hotels[$i]["description"]; ?></td>
-              <td> <?php echo $hotels[$i]["parking"] ? "Si" : "No"; ?></td>
-              <td> <?php echo $hotels[$i]["vote"]; ?></td>
-              <td> <?php echo $hotels[$i]["distance_to_center"]." km"; ?></td>
-            </tr>
-          <?php } ?>
+          <?php if (!isset($parking) || $parking == "both") { 
+            for($i=0; $i < count($hotels); $i++) { ?>
+              <tr>
+                <th> <?php echo $hotels[$i]["name"]; ?></td>
+                <td> <?php echo $hotels[$i]["description"]; ?></td>
+                <td> <?php echo $hotels[$i]["parking"] ? "Si" : "No"; ?></td>
+                <td> <?php echo $hotels[$i]["vote"]; ?></td>
+                <td> <?php echo $hotels[$i]["distance_to_center"]." km"; ?></td>
+              </tr>
+          <?php }} else {
+            for($i=0; $i < count($hotels); $i++) {
+              if($parking == ($hotels[$i]["parking"] ? "true" : "false")) { ?>
+                <tr>
+                  <th> <?php echo $hotels[$i]["name"]; ?></td>
+                  <td> <?php echo $hotels[$i]["description"]; ?></td>
+                  <td> <?php echo $hotels[$i]["parking"] ? "Si" : "No"; ?></td>
+                  <td> <?php echo $hotels[$i]["vote"]; ?></td>
+                  <td> <?php echo $hotels[$i]["distance_to_center"]." km"; ?></td>
+                </tr>            
+          <?php }}}?>
         </tbody>
       </table>
     </main>
